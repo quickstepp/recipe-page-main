c5a1099908ca0332acbf14308dbe05dcfec32605
quickstepp <github.pjwjs@slmail.me>
2024-03-11 18:22:57 +0100

    fixed style instructions

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -1,36 +1,48 @@
 export function Instructions() {
-    return (
-        <>
-            <h2>Instructions</h2>
-            <ol>
-                <li>
-                    Beat the eggs: In a bowl, beat the eggs with a pinch of salt
-                    and pepper until they are well mixed. You can add a
-                    tablespoon of water or milk for a fluffier texture.
-                </li>
-                <li>
-                    Heat the pan: Place a non-stick frying pan over medium heat
-                    and add butter or oil.
-                </li>
-                <li>
-                    Cook the omelette: Once the butter is melted and bubbling,
-                    pour in the eggs. Tilt the pan to ensure the eggs evenly
-                    coat the surface.
-                </li>
-                <li>
-                    Add fillings (optional): When the eggs begin to set at the
-                    edges but are still slightly runny in the middle, sprinkle
-                    your chosen fillings over one half of the omelette.
-                </li>
-                <li>
-                    Fold and serve: As the omelette continues to cook, carefully
-                    lift one edge and fold it over the fillings. Let it cook for
-                    another minute, then slide it onto a plate.
-                </li>
-                <li>
-                    Enjoy: Serve hot, with additional salt and pepper if needed.
-                </li>
-            </ol>
-        </>
-    );
+	return (
+		<>
+			<h2>Instructions</h2>
+			<div className="ml-12 mr-8 space-y-4 -indent-8">
+				<div>
+					<b>1. Beat the eggs:</b>
+					<br />
+					In a bowl, beat the <b>eggs</b> with a pinch of <b>salt</b>{' '}
+					and <b>pepper</b> until they are well mixed. You can add a
+					tablespoon of water or milk for a fluffier texture.
+				</div>
+				<div>
+					<b>2. Heat the pan:</b>
+					<br />
+					Place a non-stick frying pan over medium heat and add{' '}
+					<b>butter/ oil</b>.
+				</div>
+				<div>
+					<b>3. Cook the omelette:</b>
+					<br />
+					Once the butter is melted and bubbling, pour in the{' '}
+					<b>eggs</b>. Tilt the pan to ensure the eggs evenly coat the
+					surface.
+				</div>
+				<div>
+					<b>4. Add fillings (optional):</b>
+					<br />
+					When the eggs begin to set at the edges but are still
+					slightly runny in the middle, sprinkle your{' '}
+					<b>chosen fillings</b> over one half of the omelette.
+				</div>
+				<div>
+					<b>5. Fold and serve:</b>
+					<br />
+					As the omelette continues to cook, carefully lift one edge
+					and fold it over the fillings. Let it cook for another
+					minute, then slide it onto a plate.
+				</div>
+				<div>
+					<b>6. Enjoy:</b>
+					<br />
+					Serve hot, with additional salt and pepper if needed.
+				</div>
+			</div>
+		</>
+	)
 }
